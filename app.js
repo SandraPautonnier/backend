cb48d147ab5506aeceb2f273a9fd7b60d561b824
SandraPautonnier <sandrapautonnier@gmail.com>
2025-05-16 11:05:08 +0200

    Partie 3 chapitre 1

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require("express");
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb+srv://john007:john0071@cluster0-gofullstack.f1nrdbf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0-gofullstack',
-  { useNewUrlParser: true,
-    useUnifiedTopology: true })
-  .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
+const stuffRoutes = require('./routes/stuff');
+
+//const Product = require("./models/product");
+
+
+mongoose
+  .connect(
+    "mongodb+srv://john007:john0071@cluster0-gofullstack.f1nrdbf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0-gofullstack"
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
 
 const app = express();
 
@@ -24,35 +30,47 @@ app.use((req, res, next) => {
   next();
 });
 
-app.post("/api/stuff", (req, res, next) => {
-  console.log(req.body);
-  res.status(201).json({
-    message: "Objet Créé !",
-  });
+app.use('/api/stuff', stuffRoutes);
+
+/*app.get("/api/products", (req, res, next) => {
+  console.log("GET ALL");
+  Product.find()
+    .then(products => res.status(200).json({ products }))
+    .catch((error) => res.status(404).json({ error }));
 });
 
-app.get("/api/stuff", (req, res, next) => {
-  const stuff = [
-    {
-      _id: "oeihfzeoi",
-      title: "Mon premier objet",
-      description: "Les infos de mon premier objet",
-      imageUrl:
-        "https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg",
-      price: 4900,
-      userId: "qsomihvqios",
-    },
-    {
-      _id: "oeihfzeomoihi",
-      title: "Mon deuxième objet",
-      description: "Les infos de mon deuxième objet",
-      imageUrl:
-        "https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg",
-      price: 2900,
-      userId: "qsomihvqios",
-    },
-  ];
-  res.status(200).json(stuff);
+app.get("/api/products/:id", (req, res, next) => {
+  console.log("GET ONE");
+  Product.findOne({ _id: req.params.id })
+    .then(product => res.status(200).json({ product }))
+    .catch((error) => res.status(400).json({ error }));
 });
+
+app.post("/api/products", (req, res, next) => {
+  console.log("POST");
+  console.log(req.body);
+  const product = new Product({
+    ...req.body,
+  });
+  product.save()
+    .then(product => res.status(201).json({ product }))
+    .catch((error) => res.status(400).json({ error }));
+});
+
+app.put("/api/products/:id", (req, res, next) => {
+  console.log("PUT");
+  Product.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: "Modified !" }))
+    .catch((error) => res.status(400).json({ error }));
+});
+
+app.delete("/api/products/:id", (req, res, next) => {
+  console.log("DELETE");
+  Product.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: "Deleted !" }))
+    .catch((error) => res.status(400).json({ error }));
+});*/
+
+
 
 module.exports = app;
